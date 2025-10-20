5d28e06e74bbc150481e8fcdd8fcae2247a76e6b
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-10-20 18:42:47 +0200

    Mail facade

---
+++ mailbridge/mail.py
@@ -0,0 +1,16 @@
+from .mailer_factory import MailerFactory
+
+class Mail:
+    @staticmethod
+    def send(to: str, subject: str, body: str, from_email: str = None) -> bool:
+        """
+        Sends an email using the configured provider from environment variables.
+
+        :param to: Recipient email
+        :param subject: Email subject
+        :param body: HTML body of the email
+        :param from_email: Optional sender email; if None, provider default is used
+        :return: True if email was sent successfully, False otherwise
+        """
+        provider = MailerFactory.get_provider()
+        return provider.send(to=to, subject=subject, body=body, from_email=from_email)
