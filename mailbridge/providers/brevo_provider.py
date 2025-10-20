5d026e15c919f9769e3b71ad9a8b39a44c1aecad
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-10-20 18:21:32 +0200

    BrevoProvider

---
+++ mailbridge/providers/brevo_provider.py
@@ -0,0 +1,24 @@
+import requests
+from .provider_interface import ProviderInterface
+
+class BrevoProvider(ProviderInterface):
+    def __init__(self, api_key, endpoint):
+        self.api_key = api_key
+        self.endpoint = endpoint
+
+    def send(self, to, subject, body, from_email=None):
+        data = {
+            "sender": {"email": from_email},
+            "to": [{"email": to}],
+            "subject": subject,
+            "htmlContent": body,
+        }
+
+        headers = {
+            "api-key": self.api_key,
+            "Content-Type": "application/json",
+        }
+
+        resp = requests.post(self.endpoint, json=data, headers=headers)
+        resp.raise_for_status()
+        return resp.status_code in (200, 201, 202)
