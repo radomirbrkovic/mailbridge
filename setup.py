9e99b01b8566dfa51a66922b77c021a1fa0fe9ea
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-10-23 19:08:38 +0200

    Adjust setup file

--- setup.py
+++ setup.py
@@ -3,25 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name="mailbridge",
     version="0.1.0",
-    description="Flexible mail sending library for multiple providers: SMTP, SendGrid, Mailgun,  Amazon SES, Postmark, Brevo (Sendinblue)",
-    author="Radomir Brkovic",
+    author="Radomir Brković",
+    author_email="brkovic.radomir@gmail.com",
+    description="Flexible mail delivery library supporting multiple providers (SMTP, SendGrid, Mailgun, SES, Postmark, Brevo)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/radomirbrkovic/mailbridge",
     packages=find_packages(),
     install_requires=[
         "requests",
+        "boto3",
+        "python-dotenv"
     ],
-    python_requires=">=3.10",
-    extras_require={
-        "smtp": [],
-        "ses": ["boto3>=1.26.0"],
-        "sendgrid": ["requests>=2.28.0"],
-        "mailgun": ["requests>=2.28.0"],
-        "postmark": ["requests>=2.28.0"],
-        "brevo": ["requests>=2.28.0"],
-        "all": ["boto3>=1.26.0", "requests>=2.28.0"],
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    python_requires=">=3.9",
 )
